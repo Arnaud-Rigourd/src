5df5835f97b7d2a9d76074b4498197c7e1e2b44f
arnaud rigourd <arnaud.rigourd@hotmail.fr>
2023-06-01 17:18:47 +0200

    Add flatpickr package on 'contact' form to simplify the date picking

--- meetingsmanager/forms.py
+++ meetingsmanager/forms.py
@@ -1,9 +1,17 @@
 from django import forms
+from django_flatpickr.widgets import DateTimePickerInput
 
 from meetingsmanager.models import Meetings
 
 
 class CustomMeetingForm(forms.ModelForm):
+    meeting_date = forms.DateTimeField(
+        widget=DateTimePickerInput()
+    )
+
     class Meta:
         model = Meetings
         fields = ['title', 'message', 'meeting_date']
+        widgets = {
+            'meeting_date': DateTimePickerInput(),
+        }
