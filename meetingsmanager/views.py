cf95f2bbf030e3a1de49062ca6cc3433fa16bf4b
arnaud rigourd <arnaud.rigourd@hotmail.fr>
2023-06-27 11:24:19 +0200

    Add generic footer

--- meetingsmanager/views.py
+++ meetingsmanager/views.py
@@ -12,7 +12,6 @@ from meetingsmanager.forms import CustomMeetingForm, CustomMessageFormSet, Custo
 from meetingsmanager.models import Meetings, Messages
 from profilemanager.models import Company, Profile
 
-
 User = get_user_model()
 
 
@@ -21,7 +20,8 @@ class MeetingsCreate(CreateView):
     form_class = CustomMeetingForm
 
     def get_success_url(self):
-        return reverse('profilemanager:company-monitoring', kwargs={'slug': self.object.company.user.slug, 'pk': self.object.company.user.pk})
+        return reverse('profilemanager:company-monitoring',
+                       kwargs={'slug': self.object.company.user.slug, 'pk': self.object.company.user.pk})
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -79,7 +79,8 @@ class MeetingsUpdate(UpdateView):
         return super().dispatch(request, *args, **kwargs)
 
     def get_success_url(self):
-        return reverse('profilemanager:dev-meetings', kwargs={'pk': self.request.user.pk, 'slug': self.request.user.slug})
+        return reverse('profilemanager:dev-meetings',
+                       kwargs={'pk': self.request.user.pk, 'slug': self.request.user.slug})
 
     def form_valid(self, form):
         self.object = form.save(commit=False)
@@ -107,7 +108,8 @@ class MeetingsDelete(DeleteView):
         return super().dispatch(request, *args, **kwargs)
 
     def get_success_url(self):
-        return reverse('profilemanager:company-monitoring', kwargs={'slug': self.object.company.user.slug, 'pk': self.object.company.user.pk})
+        return reverse('profilemanager:company-monitoring',
+                       kwargs={'slug': self.object.company.user.slug, 'pk': self.object.company.user.pk})
 
 
 class MessagesCreate(CreateView):
